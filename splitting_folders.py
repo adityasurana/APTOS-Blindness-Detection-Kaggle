ec7de35dc38b48ff91cedebe979f0f5ecd6cc1fc
Aditya Surana <to.adityasurana@gmail.com>
2022-05-15 19:36:07 +0530

    Update splitting_folders.py

--- splitting_folders.py
+++ splitting_folders.py
@@ -1,28 +1,4 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {
-    "collapsed": true
-   },
-   "outputs": [],
-   "source": [
-    "import split_folders\n",
-    "input='dataset'\n",
-    "output='final_dataset'\n",
-    "split_folders.ratio(input, output=output, seed=1337, ratio=(.8, .1, .1))"
-   ]
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 2
-}
-#
+import split_folders
+input='dataset'
+output='final_dataset'
+split_folders.ratio(input, output=output, seed=1337, ratio=(.8, .1, .1))
